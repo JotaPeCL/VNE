b1134bad3ab328b579eddb87d86c90f9432a79d5
JotaPeCL <juanpcamlo20@gmail.com>
2024-02-13 17:46:26 -0600

    Crear proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaVNE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
